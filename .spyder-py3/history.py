e3200f71ca6e42920def52ad882ca6f3213190b5
Christian Razo <37232875+Christian-Ra@users.noreply.github.com>
2021-03-07 10:34:10 -0600

    IDk man

---
+++ .spyder-py3/history.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Mon Feb  8 23:02:23 2021)---
+runfile('C:/Users/chrra/Downloads/CS484_Assingment1_john_santiago.py', wdir='C:/Users/chrra/Downloads')
